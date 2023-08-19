40793efd69e0f7a01b669bab80cb91e4a3d500d8
felipe silva <1felipersilva1@gmail.com>
2023-08-19 18:48:23 -0300

    Handling errors and doing password encryption.

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -1,17 +1,34 @@
+import { AppError } from "../utils/AppErros.js";
 import { prismaClient } from "../utils/prismaClient.js";
-
+import bcryptjs from "bcryptjs";
 
 export class UserController{
-  async create(request) {
+  async create(request, reply) {
     const { name, email, password } = request.body;
+
+    if (!name || !password) {
+      throw new AppError("Invalid username or password");
+ 
+    }
+    if (!email) {
+      throw new AppError("Invalid email");
+    }
+
+    // const checkUserExists = await database.get(
+    //   `SELECT email FROM users WHERE email = '${email}'`
+    // );
+    // if (checkUserExists) {
+    //   throw new AppError("Email already exists");
+    // }
+    const hashedPassword = await bcryptjs.hash(password, 8);
     
-    const user = await prismaClient.user.create({
+    await prismaClient.user.create({
       data: {
         name,
         email,
-        password
+        password: hashedPassword
       }
     })
-    return user
+    reply.code(201)
   }
 }

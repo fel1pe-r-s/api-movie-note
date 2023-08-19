baa7714eb2f4ed0b5f99f3b765a71ae544804d61
felipe silva <1felipersilva1@gmail.com>
2023-08-19 20:43:12 -0300

    Checking if email has already been registered

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -2,33 +2,34 @@ import { AppError } from "../utils/AppErros.js";
 import { prismaClient } from "../utils/prismaClient.js";
 import bcryptjs from "bcryptjs";
 
-export class UserController{
+export class UserController {
   async create(request, reply) {
     const { name, email, password } = request.body;
-
     if (!name || !password) {
       throw new AppError("Invalid username or password");
- 
     }
     if (!email) {
       throw new AppError("Invalid email");
     }
-
-    // const checkUserExists = await database.get(
-    //   `SELECT email FROM users WHERE email = '${email}'`
-    // );
-    // if (checkUserExists) {
-    //   throw new AppError("Email already exists");
-    // }
+    const checkUserExists = await prismaClient.user.findUnique({
+      where: { email },
+    });
+    if (checkUserExists) {
+      throw new AppError("email already exists");
+    }
     const hashedPassword = await bcryptjs.hash(password, 8);
-    
-    await prismaClient.user.create({
-      data: {
-        name,
-        email,
-        password: hashedPassword
-      }
-    })
-    reply.code(201)
+    try {
+      await prismaClient.user.create({
+        data: {
+          name,
+          email,
+          password: hashedPassword,
+        },
+      });
+      prismaClient.$disconnect();
+    } catch (error) {
+      reply.code({ error });
+    }
+    reply.code(201);
   }
 }
